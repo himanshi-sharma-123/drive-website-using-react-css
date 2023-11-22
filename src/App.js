63152300bb0af9db6e79bc7434fc46f776fb0fe9
Himanshi Sharma <himanshish456@gmail.com>
2023-11-22 12:00:47 +0530

    driver component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Find from "./components/Find/Find";
+import Driver from "./components/Driver/Driver";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Find />
+      <Driver />
     </div>
   );
 }
